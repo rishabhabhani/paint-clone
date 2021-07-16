d1b1227faac67665be20a7129851f7c3ba407a2c
Rishabh Abhani <rishabhabhani@gmail.com>
2021-07-16 14:27:58 +0530

    base settings

---
+++ utils/settings.py
@@ -0,0 +1,27 @@
+import pygame
+pygame.init()
+pygame.font.init()
+
+WHITE = (225, 225, 225)
+BLACK = (25, 25, 25)
+RED = (225, 0, 0)
+BLUE = (0, 225, 0)
+GREEN = (0, 0, 225)
+
+FPS = 60
+
+WIDTH, HEIGHT = 600, 700
+
+ROWS = COLS = 50
+
+TOOLBAR_HEIGHT = HEIGHT - WIDTH
+
+PIXEL_SIZE = WIDTH // COLS
+
+BG_COLOR = WHITE
+
+DRAW_GRID_LINES = False
+
+
+def get_font(size):
+    return pygame.font.SysFont("comicsans", size)
